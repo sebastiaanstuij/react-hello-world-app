320c6a480132339e20269961744960060a78c949
Sebastiaan Stuij <sebastiaan@Sebastiaans-MacBook-Pro.local>
2017-08-05 14:04:21 +0200

    completed tutorial

---
+++ src/HelloWorld.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './HelloWorld.css';
+
+class HelloWorld extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { greeting: 'Hello' };
+        this.frenchify = this.frenchify.bind(this);   
+        
+        this.removeGreeting = this.removeGreeting.bind(this);        
+    }
+
+    render() {
+        return (
+            <div className="HelloWorld">
+                {this.state.greeting} {this.props.name}!
+                <br/>
+                <button className="frenchify" onClick={this.frenchify}>Frenchify!</button>
+                <br/>
+                <button className="remove" onClick={this.removeGreeting}>Remove Me!</button>
+            </div>
+        );
+    }
+
+    frenchify() {
+        this.setState({ greeting: 'Bonjour' });
+    }
+
+    removeGreeting() {
+        this.props.removeGreeting(this.props.name);
+    }
+}
+
+export default HelloWorld;
